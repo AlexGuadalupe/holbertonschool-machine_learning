2f89c358f7aedd43233ab33584f8fc6156a62faa
Alex Guadalupe <9035@holbertonstudents.com>
2024-12-02 12:21:59 -0500

    fix code task 0

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # Array donde nos vamos a estar moviendo #
+arr = [9, 8, 2, 3, 9, 4, 1, 0, 3] # Array donde nos vamos a estar moviendo #
 arr1 = arr [:2] # Seleccion de los primeros dos elementos #
 arr2 = arr[-5:] # Seleccion de los ultimos cinco elementos #
 arr3 = arr[1:6] # Seleccion de los elementos del 2 al 6 #
