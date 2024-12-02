cc6f13099392d51556bb30995cd0d2f15c5d6612
Alex Guadalupe <9035@holbertonstudents.com>
2024-12-02 12:06:36 -0500

    addin task0

---
+++ math/linear_algebra/0-slice_me_up.py
@@ -0,0 +1,7 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # Array donde nos vamos a estar moviendo #
+arr1 = arr [:2] # Seleccion de los primeros dos elementos #
+arr2 = arr[-5:] # Seleccion de los ultimos cinco elementos #
+arr3 = arr[1:6] # Seleccion de los elementos del 2 al 6 #
+print("arr1:", arr1) # Impresion de los primeros dos elementos #
+print("arr2:", arr2) # Impresion de los ultimos cinco elementos #
+print("arr3:", arr3) # Impresion de los elementos del 2 al 6 #
