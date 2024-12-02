04926974eb1e838b27477de185374da1ed476932
Alex Guadalupe <9035@holbertonstudents.com>
2024-12-02 17:06:46 -0500

    fix code task 0

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 = arr [:2] # Seleccion de los primeros dos elementos #
-arr2 = arr[-5:] # Seleccion de los ultimos cinco elementos #
-arr3 = arr[1:6] # Seleccion de los elementos del 2 al 6 #
+arr1 = arr[:2]  # Seleccion de los primeros dos elementos #
+arr2 = arr[-5:]  # Seleccion de los ultimos cinco elementos #
+arr3 = arr[1:6]  # Seleccion de los elementos del 2 al 6 #
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
