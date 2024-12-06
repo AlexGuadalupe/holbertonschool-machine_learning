8caddc19da99235d6721c19e916385274dc0da43
Alex Guadalupe <9035@holbertonstudents.com>
2024-12-06 15:07:26 -0500

    adding task 9 after pycodestyle check

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -6,5 +6,5 @@ matrix = np.array([
     [11, 12, 13, 14, 15],
     [16, 17, 18, 19, 20],
     [21, 22, 23, 24, 25]])
-mat1, mat2, mat3 = matrix[1:3, :], matrix[:, 2:4], matrix[2:5, 2:5]
-print("mat1:\n", mat1, "\nmat2:\n", mat2, "\nmat3:\n", mat3)
+mat1, mat2, mat3 = matrix[1:3, :], matrix[:, 1:3], matrix[2:5, 2:5]
+print("The middle two rows of the matrix are:\n", mat1, "\nThe middle two columns of the matrix are:\n", mat2, "\nThe bottom-right, square, 3x3 matrix is:\n", mat3)
