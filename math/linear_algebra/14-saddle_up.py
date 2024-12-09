3c1d4e720113b0143b4874c00b0ea9c8dbf2e3b8
Alex Guadalupe <9035@holbertonstudents.com>
2024-12-08 22:32:50 -0400

    task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" module that performs matrix multiplication: """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ performs matrix multiplication
+    with no loops or cond. statements """
+    return mat1 @ mat2
