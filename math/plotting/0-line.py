5dc53d91350042fabc41a9f6337a735693d242c3
Alex Guadalupe <9035@holbertonstudents.com>
2024-12-15 18:31:41 -0400

    fix task0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python3
+""" plot y as a line graph: """
+import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
-    # Datos
-    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # Valores de x del 0 al 10
-    y = [0, 1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]  # Valores de y (cubos de x)
+    """ plot y as a line graph: """
 
-    # Crear la gráfica
-    plt.plot(x, y, color='red', linestyle='-', label='y = x^3')  # Línea sólida roja
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
 
-    # Etiquetas y título
-    plt.xlabel('Eje x')  # Etiqueta del eje x
-    plt.ylabel('Eje y')  # Etiqueta del eje y
-    plt.title('Gráfica de y = x^3')  # Título
+    plt.plot(y, "r-")  # plot y as a red line
+    plt.xlim([0, 10])  # sets x axis from 0
 
-    # Definir límites del eje x
-    plt.xlim(0, 10)
-
-    # Mostrar la gráfica
-    plt.legend()  # Mostrar la leyenda
-    plt.grid(True)  # Activar la cuadrícula
-    plt.show()
-
-if __name__ == "__main__":
-    line()
+    plt.show()  # this displays the plot
